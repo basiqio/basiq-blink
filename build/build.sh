2b6eac941f0d5abe33c398ae46d77e5e64f3e850
Stefan Lapcevic <stefan.lapcevic@basiq.io>
2018-05-24 11:42:53 +0200

    Add build infrastructure to basiq blink

---
+++ build/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf dist
+mkdir -p dist
+
+cp -r static/fonts dist
+cp -r static/js dist
+cp -r static/js/polyfills dist
+cp static/fortress.svg dist
+cp static/index.html dist
+cp static/institution.svg dist
+cp static/style.css dist
+cp static/js/helpers.js dist
