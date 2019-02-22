6e3835d652ad13151ae28d33cdca37240cf44aee
Aleksandar Terentic <aleksandar@basiq.io>
2019-02-22 10:58:55 +0100

    Copy whole static folder to dist

--- build/build.sh
+++ build/build.sh
@@ -2,14 +2,4 @@
 
 rm -rf dist
 mkdir -p dist
-mkdir -p dist/images
-
-cp -r static/fonts dist
-cp -r static/js dist
-cp -r static/js/polyfills dist
-cp static/images/fortress.svg dist/images
-cp static/index.html dist
-cp static/images/institution.svg dist/images
-cp static/style.css dist
-cp static/js/helpers.js dist
-cp static/js/API.js dist
+cp -r static/* dist
