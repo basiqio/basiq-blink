50ded071cd84269f247c7830a657384229d5cbdc
djordjebasiq <djordje@basiq.io>
2019-02-28 10:14:19 +0100

    Add logs inside bash scripts

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Starting build..."
 rm -rf dist
 mkdir -p dist
 cp -r static/* dist
 echo "window.basiqConfig = " > dist/config.js
 build/read-environment.sh >> dist/config.js
+echo "Build ended"
--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+echo "Starting deploy..."
 export Bucket=`aws cloudformation list-exports --output text --query "Exports[?(Name=='basiq-blink-s3-bucket')].Value"`
 aws s3 sync dist s3://$Bucket --acl public-read
+echo "Deploy ended"
