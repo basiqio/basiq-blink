1ea3978dfb11f9860cc6bb8e1c9dbfd9cf869d51
stefanacerovina <stefana@basiq.io>
2019-06-27 13:02:15 +0200

    Added support for csv files

--- static/js/pages/pdfUpload.js
+++ static/js/pages/pdfUpload.js
@@ -3,23 +3,23 @@
 var host = readConfig("basiq-api-host");
 var notSupportedFileInList = false;
 
-function notSupportedFilesOrEmptyList(files){
-    if(files.length == 0){
+function notSupportedFilesOrEmptyList(files) {
+    if (files.length == 0) {
         return true
     }
 
-    return files.some(function (file){
+    return files.some(function (file) {
         var fileExt = file.name.split('.').pop()
 
-        if(fileExt){
+        if (fileExt) {
             fileExt = fileExt.toLowerCase()
         }
 
-        if(fileExt == "pdf"){
+        if (["pdf", "csv"].includes(fileExt)) {
             return getFileSizeInMB(file.size) > 10
         }
 
-        return fileExt != "pdf"
+        return !["pdf", "csv"].includes(fileExt)
     })
 }
 
@@ -27,7 +27,7 @@ function getFileSizeInMB(bytes) {
     if (bytes === 0) return 0;
 
     const k = 1024;
-    return bytes/k/k;
+    return bytes / k / k;
 
 }
 
@@ -51,7 +51,7 @@ window.pages["pdfUpload"] = function (container, institution) {
     dropzoneContainer.id = "dropzone";
     dropzoneContainer.className = "dropzoneContainer dropzone";
 
-    var logoContainer = document.createElement("div"), 
+    var logoContainer = document.createElement("div"),
         logo = document.createElement("img");
 
     logoContainer.className = "bank-logo-container";
@@ -78,24 +78,24 @@ window.pages["pdfUpload"] = function (container, institution) {
 
     previewsContainer.className = "dropzone-previews";
     previewsContainer.id = "previews";
- 
+
     var pdfDropzone = new Dropzone(dropzoneContainer, {
         url: host + "/users/" + window.globalState.userId + "/statements",
         previewsContainer: previewsContainer,
         parallelUploads: 3,
-        previewTemplate: "<div id=\"tpl\" class=\"dz-preview dz-file-preview\">\n" + 
-            "<div class=\"dz-details\"><div id=\"pdficon\" class=\"dz-pdf-icon\"></div>\n<div class=\"dz-filename\"><span data-dz-name></span></div></div>\n" + 
+        previewTemplate: "<div id=\"tpl\" class=\"dz-preview dz-file-preview\">\n" +
+            "<div class=\"dz-details\"><div id=\"pdficon\" class=\"dz-pdf-icon\"></div>\n<div class=\"dz-filename\"><span data-dz-name></span></div></div>\n" +
             "<div class=\"dz-progress\"><span class=\"dz-upload\" data-dz-uploadprogress></span></div><a href=\"javascript:null;\" class=\"dz-remove-file\" data-dz-remove>X</a>\n",
         dictDefaultMessage: "<h4>Drag & Drop or <span>Browse</span></h4>We support only official statements downloaded directly from your banking institution",
         createImageThumbnails: false,
-        acceptedFiles: "application/pdf",
+        acceptedFiles: "application/pdf,text/csv",
         autoProcessQueue: false,
         paramName: "statement",
         errorResolver: function (_, response) {
             if (!response.data || !response.data[0]) {
                 return "Unknown error while uploading file";
             }
-            return response.data[0].title + " " + response.data[0].detail; 
+            return response.data[0].title + " " + response.data[0].detail;
         },
         headers: {
             "Authorization": "Bearer " + window.globalState.accessToken
@@ -109,13 +109,13 @@ window.pages["pdfUpload"] = function (container, institution) {
     window.jobIds = [];
     pdfDropzone.on("success", function (event, response) {
         window.jobIds.push(response.id);
-        sendEventNotification("job", { success: true, data: { id: response.id, institutionShortName: event.institution.shortName, fileName: event.name }});
+        sendEventNotification("job", { success: true, data: { id: response.id, institutionShortName: event.institution.shortName, fileName: event.name } });
         if (response.links && response.links.self) {
             window.jobs.push(response.links.self);
         }
     });
 
-    pdfDropzone.on("error", function(response, error){
+    pdfDropzone.on("error", function (response, error) {
         notSupportedFileInList = true;
         // var err = error && error.data && error.data[0] ? error.data[0] : {detail: "Unknown error"};
         // transitionToPage("loading", err);
@@ -123,7 +123,7 @@ window.pages["pdfUpload"] = function (container, institution) {
 
     pdfDropzone.on("queuecomplete", function () {
         if (window.globalState.ignoreParsing) {
-            if(notSupportedFileInList){
+            if (notSupportedFileInList) {
                 return;
             }
             transitionToPage("pdfResult", "success", institution, []);
@@ -131,55 +131,55 @@ window.pages["pdfUpload"] = function (container, institution) {
         }
 
         var promises = [];
-        window.jobIds.forEach(function(jobId){
-            promises.push(new Promise(function(resolve, reject){
-            function checkJobStatus(timeout) {
-                var now = new Date().getTime();
-                window.API.checkJobStatus(window.globalState.accessToken, jobId).then(function (resp) {
-                    var steps = resp.steps;
-                    for (var step in steps) {
-                        if (!steps.hasOwnProperty(step)) {
-                            continue;
-                        }
-                        if (steps[step].title === "verify-credentials") {
-                            switch (steps[step].status) {
-                                case "failed":
-                                   return  resolve({status:"failure", step: steps[step]});
-                                case "success":
-                                   return  resolve({status:"success", step: steps[step]});
-                                case "pending":
-                                case "in-progress":
-                                    new Date().getTime() - now > timeout ? resolve({status: "failure", step:step[step]}) : setTimeout(checkJobStatus, 1000);
+        window.jobIds.forEach(function (jobId) {
+            promises.push(new Promise(function (resolve, reject) {
+                function checkJobStatus(timeout) {
+                    var now = new Date().getTime();
+                    window.API.checkJobStatus(window.globalState.accessToken, jobId).then(function (resp) {
+                        var steps = resp.steps;
+                        for (var step in steps) {
+                            if (!steps.hasOwnProperty(step)) {
+                                continue;
                             }
-            
+                            if (steps[step].title === "verify-credentials") {
+                                switch (steps[step].status) {
+                                    case "failed":
+                                        return resolve({ status: "failure", step: steps[step] });
+                                    case "success":
+                                        return resolve({ status: "success", step: steps[step] });
+                                    case "pending":
+                                    case "in-progress":
+                                        new Date().getTime() - now > timeout ? resolve({ status: "failure", step: step[step] }) : setTimeout(checkJobStatus, 1000);
+                                }
+
+                            }
                         }
-                    }
-                }).catch(function (err) {
-                   reject(err.message);
-                });
-            }
-            checkJobStatus(180000);
+                    }).catch(function (err) {
+                        reject(err.message);
+                    });
+                }
+                checkJobStatus(180000);
             }));
         });
-        if(promises.length > 0 && !notSupportedFileInList){
+        if (promises.length > 0 && !notSupportedFileInList) {
             transitionToPage("pdfResult", "loading", institution);
-            Promise.all(promises).then(function(results){
+            Promise.all(promises).then(function (results) {
                 var status = "success";
                 var steps = [];
-                results.forEach(function(result){
-                    if(result.status === "failure") {
+                results.forEach(function (result) {
+                    if (result.status === "failure") {
                         status = "failure";
                     }
                     steps.push(result.step);
                 });
                 transitionToPage("pdfResult", status, institution, steps);
-            }).catch(function(err){
+            }).catch(function (err) {
                 transitionToPage("loading", err);
             });
         }
     });
 
-    pdfDropzone.on("addedfile", function(file) {
+    pdfDropzone.on("addedfile", function (file) {
         notSupportedFileInList = notSupportedFilesOrEmptyList(pdfDropzone.files)
 
         var fileSize = getFileSizeInMB(file.size)
@@ -187,42 +187,42 @@ window.pages["pdfUpload"] = function (container, institution) {
 
         file.previewTemplate.style.color = "#65828C";
 
-        if(fileExt){
+        if (fileExt) {
             fileExt = fileExt.toLowerCase()
         }
 
-        if(fileExt != "pdf"){
+        if (!["pdf", "csv"].includes(fileExt)) {
             setNotValidItem(file, "Not a supported file type.")
         }
 
-        if(fileExt == "pdf" && fileSize > 10){
+        if (["pdf", "csv"].includes(fileExt) && fileSize > 10) {
             setNotValidItem(file, "File is bigger then 10MB.")
         }
 
-        if(notSupportedFileInList == false){
+        if (notSupportedFileInList == false) {
             document.getElementById("footer").innerHTML = "";
             setActiveButton2("Continue", function (button) {
                 button.disabled = true;
-                window.filesToUpload = window.filesToUpload.concat(pdfDropzone.files.map(function (file) {file.institution = institution; return file;}));
+                window.filesToUpload = window.filesToUpload.concat(pdfDropzone.files.map(function (file) { file.institution = institution; return file; }));
                 pdfDropzone.processQueue();
             });
         }
-        else{
+        else {
             hideAllButtons()
         }
     });
 
-    pdfDropzone.on("removedfile", function(file) {
+    pdfDropzone.on("removedfile", function (file) {
         notSupportedFileInList = notSupportedFilesOrEmptyList(pdfDropzone.files)
-        if(notSupportedFileInList == false){
+        if (notSupportedFileInList == false) {
             document.getElementById("footer").innerHTML = "";
             setActiveButton2("Proceed", function (button) {
                 button.disabled = true;
-                window.filesToUpload = window.filesToUpload.concat(pdfDropzone.files.map(function (file) {file.institution = institution; return file;}));
+                window.filesToUpload = window.filesToUpload.concat(pdfDropzone.files.map(function (file) { file.institution = institution; return file; }));
                 pdfDropzone.processQueue();
             });
         }
-        else{
+        else {
             hideAllButtons()
         }
     });
@@ -234,29 +234,29 @@ window.pages["pdfUpload"] = function (container, institution) {
     return pageContainer;
 };
 
-function setNotValidItem(file, text){
+function setNotValidItem(file, text) {
     details = document.querySelectorAll(".dz-details")
-    details.forEach(detail => { 
-            fileNameElement = detail.querySelector(".dz-filename"); 
-            if(file.name === fileNameElement.innerText){ 
+    details.forEach(detail => {
+        fileNameElement = detail.querySelector(".dz-filename");
+        if (file.name === fileNameElement.innerText) {
 
-                var pdfIcon = detail.querySelector("#pdficon")
+            var pdfIcon = detail.querySelector("#pdficon")
 
-                pdfIcon.className = ""
-                pdfIcon.style.marginRight = 8
-                pdfIcon.style.marginLeft = 8
-                pdfIcon.innerHTML = "?"
+            pdfIcon.className = ""
+            pdfIcon.style.marginRight = 8
+            pdfIcon.style.marginLeft = 8
+            pdfIcon.innerHTML = "?"
 
-                var divElement = document.createElement('div')
-                divElement.style.marginTop = 4
-                var spanElement = document.createElement('span')
-                spanElement.style.fontWeight = 100
-                spanElement.style.opacity = 0.9
-                spanElement.innerText = text
-                divElement.appendChild(spanElement)
-                fileNameElement.appendChild(divElement)
-            }
-        })
+            var divElement = document.createElement('div')
+            divElement.style.marginTop = 4
+            var spanElement = document.createElement('span')
+            spanElement.style.fontWeight = 100
+            spanElement.style.opacity = 0.9
+            spanElement.innerText = text
+            divElement.appendChild(spanElement)
+            fileNameElement.appendChild(divElement)
+        }
+    })
 
     file.previewTemplate.style.color = "#E24A4A";
 }
